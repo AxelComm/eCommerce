7e8a97cebc91d0b2d9f21ee5006458a672ddea69
AxelComm <commerly.axel@outlook.fr>
2023-07-23 14:54:36 +0200

    commit

--- assets/app.js
+++ assets/app.js
@@ -3,4 +3,6 @@ import './scss/app.scss';
 import './scss/profil.scss'
 import './scss/shop.scss'
 import './scss/home.scss'
+import './scss/apropos.scss'
+import './scss/produitdetail.scss'
 import * as bootstrap from 'bootstrap'
