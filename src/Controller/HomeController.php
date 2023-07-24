1288709eaf7d5ad85bf6a86c650936911ef3e3f4
AxelComm <commerly.axel@outlook.fr>
2023-07-24 17:19:34 +0200

    commit

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AccueilController extends AbstractController
+class HomeController extends AbstractController
 {
     #[Route('/accueil', name: 'app_accueil')]
     public function index(): Response
     {
         return $this->render('accueil/index.html.twig', [
-            'controller_name' => 'AccueilController',
+            'controller_name' => 'HomeController',
         ]);
     }
 }
